cb890f0e748d8f32bd8c65b1ccf8ef684299c7ea
hikari <xxhyxayxx0619@gmail.com>
2021-06-17 19:28:25 +0900

    first commit

---
+++ 3d.js
@@ -0,0 +1,25 @@
+function Three(){
+    //vis name
+    this.name = "3D";
+
+    this.draw = function(){
+        push();
+        noStroke();
+        translate(width/2,height/2,0);
+        pointLight(255, 0, 106, -200, 0, 0);
+        pointLight(0, 0, 255, 200, 0, 0);
+        ambientLight(30);
+        fourier.analyze();
+        let bass = fourier.getEnergy("bass");
+        let level = amplitude.getLevel();
+        rotateX(frameCount * 0.01);
+        rotateY(frameCount * 0.01);
+        //ambientMaterial(88, 170, 252);
+        //fill(249, 129, 201);
+        //box(25 * (bass / 100));
+        normalMaterial();
+        //ambientMaterial(250);
+        torus(100 + (level * 200), 50 + (level * 100));
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,60 @@
+//global for the controls and input 
+var controls = null;
+//store visualisations in a container
+var vis = null;
+//variable for the p5 sound object
+var sound = null;
+//variable for p5 fast fourier transform
+var fourier;
+var amplitude;
+var t;
+function preload(){
+	sound = loadSound('assets/IMG_1196.mp4');
+	font = loadFont('assets/Roboto-Regular.ttf');
+}
+
+function setup(){
+	 background(0);
+	 createCanvas(windowWidth, windowHeight, WEBGL);
+	 t = createGraphics(windowWidth, windowHeight);
+	 controls = new ControlsAndInput();
+	 textFont(font);
+	 //instantiate the fft object
+	 fourier = new p5.FFT();
+	 amplitude = new p5.Amplitude();
+
+	 //create a new visualisation container and add visualisations
+	 vis = new Visualisations();
+	 vis.add(new Spectrum());
+	 vis.add(new WavePattern());
+	 vis.add(new Needles());
+	 vis.add(new Circle());
+	 vis.add(new Three());
+
+}
+
+function draw(){
+	background(0);
+	translate(-width/2,-height/2,0);
+	//draw the selected visualisation
+	vis.selectedVisual.draw();
+	//draw the controls on top.
+	controls.draw();
+}
+
+function mouseClicked(){
+	controls.mousePressed();
+}
+
+function keyPressed(){
+	controls.keyPressed(keyCode);
+}
+
+//when the window has been resized. Resize canvas to fit 
+//if the visualisation needs to be resized call its onResize method
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+	if(vis.selectedVisual.hasOwnProperty('onResize')){
+		vis.selectedVisual.onResize();
+	}
+}
