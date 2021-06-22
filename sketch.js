2864c5693a8720415f0063357fd0ea15ab4531cd
hikari <xxhyxayxx0619@gmail.com>
2021-06-23 08:12:57 +0900

    :fire: delete test code

--- sketch.js
+++ sketch.js
@@ -9,21 +9,14 @@ let fourier;
 let amplitude;
 let t;
 
-var Engine = Matter.Engine,
-	//Render = Matter.Render,
-	Runner = Matter.Runner,
-	Bodies = Matter.Bodies,
-	Body = Matter.Body,
-	Composite = Matter.Composite;
+const Engine = Matter.Engine,
+	  Bodies = Matter.Bodies,
+	  Body = Matter.Body,
+	  Composite = Matter.Composite;
 
-let engine,boxy,composite,ground,a,top_wall,left,right;
-let boxes = [];
-
-let ball;
-let level,value_s;
+let engine,composite,ground,a,top_wall,left,right;
 
 let box_arry, num_box;
-
 
 function preload(){
 	sound = loadSound('assets/sound/stomper_reggae_bit.mp3');
@@ -33,31 +26,31 @@ function preload(){
 function setup(){
 	 background(0);
 	 createCanvas(windowWidth, windowHeight, WEBGL);
-	 t = createGraphics(windowWidth, windowHeight);
-	 controls = new ControlsAndInput();
 	 textFont(font);
+
+	 //for circle.js
+	 t = createGraphics(windowWidth, windowHeight);
+
+	 controls = new ControlsAndInput();
+
 	 //instantiate the fft object
 	 fourier = new p5.FFT();
 	 amplitude = new p5.Amplitude();
 
+	 //for physics.js
 	 engine = Engine.create();
 	 composite = engine.world;
 	 Engine.run(engine);
-	 boxy = new Physics_box(200,100,50,50);
 	 ground = Bodies.rectangle(0, height, width*2, 100, { isStatic: true });
 	 top_wall = Bodies.rectangle(0, 0, width*2, 20, { isStatic: true });
 	 left = Bodies.rectangle(0,0,20,30,{ isStatic: true });
 	 right = Bodies.rectangle(750,210,20,300,{ isStatic: true })
-	 //ball = Bodies.circle(200,200,20);
-	 //Composite.add(composite, [ground,ball,left,right,top]);
 	 Composite.add(composite, [ground,top_wall,left,right]);
-
-	box_arry = []
-	num_box = 250;
-
-	for(let i = 0; i < num_box; i++){
+	 box_arry = []
+	 num_box = 250;
+	 for(let i = 0; i < num_box; i++){
 		box_arry.push(new Physics_box(random(0,width), random(0,height), 20, 20));
-	}
+	 }
 
 	 //create a new visualisation container and add visualisations
 	 vis = new Visualisations();
@@ -77,36 +70,14 @@ function draw(){
 	vis.selectedVisual.draw();
 	//draw the controls on top.
 	controls.draw();
-	// for(i = 0; i < boxes.length; i++){
-	// 	boxes[i].show();
-	// }
-	//ellipse(ball.position.x,ball.position.y,20);
-	//boxy.show;
-	//fourier.analyze();
-	//level = amplitude.getLevel();
-	//value_s = level*-0.1
-	//console.log(boxes);
-	//boxy.show();
 }
 
 function mouseClicked(){
 	controls.mousePressed();
 }
 
-function mouseDragged(){
-	//boxes.push(new Physics_box(mouseX, mouseY, 20, 20));
-}
-
 function keyPressed(){
 	controls.keyPressed(keyCode);
-	//console.log(level * -0.1);
-	// for(i = 0; i < boxes.length; i++){
-	// 	boxes[i].mova();
-	// 	console.log(boxes[i]);
-	// }
-
-	//Body.applyForce( ball, {x: ball.position.x, y: ball.position.y}, {x: 0, y: -0.05});
-
 }
 
 //when the window has been resized. Resize canvas to fit
